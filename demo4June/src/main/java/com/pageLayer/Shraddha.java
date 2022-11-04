43c053559645b85d77a95cbfb89c570e7e86e1f4
2590Shraddha <shraddha.tilekar25@gmail.com>
2022-11-04 19:18:27 +0530

    add new class 'shraddha'
    please check and reviewd

---
+++ demo4June/src/main/java/com/pageLayer/Shraddha.java
@@ -0,0 +1,13 @@
+package com.pageLayer;
+
+import org.openqa.selenium.support.PageFactory;
+
+import com.testBase.TestBaseDemo;
+
+public class Shraddha extends TestBaseDemo
+{
+	public Shraddha()
+	{
+		PageFactory.initElements(driver, this);
+	}
+}
